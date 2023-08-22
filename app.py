485e2da6d02f08b32a54bbcc8b2986244c9ecdff
TGR1621 <119565536+TGR1621@users.noreply.github.com>
2023-08-22 09:49:16 +0700

    streamlit_app.py

--- app.py
+++ app.py
@@ -1,94 +1,59 @@
-from flask import Flask, request, render_template
+import streamlit as st
 import pickle
 import locale
 import numpy as np
 import pandas as pd
-import streamlit as st
 
-app = Flask(__name__)
-
-# Memuat model Regresi Linier dan Random Forest dari file pickle
+# Load models and dataset
 with open('linreg_model.pkl', 'rb') as f:
     linreg_model = pickle.load(f)
 
 with open('rf_model.pkl', 'rb') as f:
     rf_model = pickle.load(f)
 
-# Membaca Data Set
 dataset = pd.read_csv('DATA RUMAH.csv', sep=';')
 dataset = dataset.rename(columns={'NAMA RUMAH': 'NAMA_RUMAH'})
 dataset = dataset.drop(columns='NO')
 
-# Atur locale ke bahasa Indonesia
+# Set locale and currency format
 locale.setlocale(locale.LC_ALL, 'id_ID')
-
-# Set format mata uang sebagai Rupiah
 currency_format = locale.currency
 
-# Fungsi untuk menghitung rekomendasi rumah berdasarkan hasil prediksi
-def convert_to_float(value):
-    if isinstance(value, str):
-        value = value.replace('Rp', '').replace('.', '').replace(',', '.')
-    try:
-        return float(value)
-    except ValueError:
-        return np.nan
+# Define functions as before...
 
-def calculate_similarity(predicted_price, actual_price):
-    predicted_price = convert_to_float(predicted_price)  # Convert to float
-    actual_price = convert_to_float(actual_price)  # Convert to float
+def main():
+    st.title('Rumah Recommendation App')
 
-    similarity = 100 - (abs(predicted_price - actual_price) / actual_price) * 100
-    return similarity
+    # Input form
+    lb = st.number_input('Lebar Bangunan (lb)')
+    lt = st.number_input('Luas Tanah (lt)')
+    kt = st.number_input('Jumlah Kamar Tidur (kt)', step=1)
+    km = st.number_input('Jumlah Kamar Mandi (km)', step=1)
+    GRS = st.number_input('Luas Garasi (GRS)', step=1)
 
-def get_rekomendasi(linreg_prediction, rf_prediction, num_samples=5):
-    # Convert predictions to float using the custom function
-    linreg_prediction = convert_to_float(linreg_prediction)
-    rf_prediction = convert_to_float(rf_prediction)
-
-    # Calculate similarity between predicted prices and prices in the dataset
-    dataset['Similarity_linreg'] = dataset.apply(
-        lambda row: calculate_similarity(linreg_prediction, row['HARGA']), axis=1)
-    dataset['Similarity_rf'] = dataset.apply(
-        lambda row: calculate_similarity(rf_prediction, row['HARGA']), axis=1)
-
-    # Combine the similarities and sort the dataset based on the similarity
-    dataset['Similarity'] = (dataset['Similarity_linreg'] + dataset['Similarity_rf']) / 2
-    dataset.sort_values(by='Similarity', ascending=False, inplace=True)
-
-    # Get the random recommendations
-    random_rekomendasi = dataset.sample(n=num_samples, random_state=42)
-
-    return random_rekomendasi.to_dict('records')
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    if request.method == 'POST':
-        lb = float(request.form['lb'])
-        lt = float(request.form['lt'])
-        kt = int(request.form['kt'])
-        km = int(request.form['km'])
-        GRS = int(request.form['GRS'])
-
+    if st.button('Predict'):
         input_data = [[lb, lt, kt, km, GRS]]
-        
-          # Get predictions for each model
+
+        # Get predictions for each model
         linreg_prediction = linreg_model.predict(input_data)[0]
         rf_prediction = rf_model.predict(input_data)[0]
 
-        # Ubah hasil prediksi menjadi format mata uang Rupiah
-        linreg_prediction = currency_format(linreg_prediction, grouping=True)
-        rf_prediction = currency_format(rf_prediction, grouping=True)
-      
-       # Get random recommended houses based on the input data and predicted prices
-        rekomendasi = get_rekomendasi(linreg_prediction, rf_prediction, num_samples=5)
+        # Convert predictions to currency format
+        linreg_prediction_str = currency_format(linreg_prediction, grouping=True)
+        rf_prediction_str = currency_format(rf_prediction, grouping=True)
 
-        return render_template('index.html', linreg_prediction=linreg_prediction, rf_prediction=rf_prediction,
-                               rekomendasi=rekomendasi, hasil_input=request.form)
+        # Get recommended houses
+        rekomendasi = get_rekomendasi(linreg_prediction_str, rf_prediction_str, num_samples=5)
+
+        # Display predictions and recommendations
+        st.subheader('Predictions:')
+        st.write(f'Linear Regression Prediction: {linreg_prediction_str}')
+        st.write(f'Random Forest Prediction: {rf_prediction_str}')
+
+        st.subheader('Recommended Houses:')
+        for house in rekomendasi:
+            st.write(f"House: {house['NAMA_RUMAH']}, Price: {house['HARGA']}")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    main()
+
